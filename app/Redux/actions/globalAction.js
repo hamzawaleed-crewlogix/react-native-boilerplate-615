ee5d80c069db035f3180822fa6ef80f17a722506
Hamza Waleed <hamza.waleed@crewlogix.com>
2019-12-18 13:56:42 +0500

    Linted

--- app/Redux/actions/globalAction.js
+++ app/Redux/actions/globalAction.js
@@ -1,50 +1,50 @@
 export const setLanguageAndCache = (lang, cache, styles) => {
   return {
-    type: "SET_LANG_&_CACHE",
+    type: 'SET_LANG_&_CACHE',
     lang: lang,
     cache: cache,
-    styles: styles
+    styles: styles,
   };
 };
 
 export const setCache = cache => {
   return {
-    type: "SET_CACHE",
-    cache: cache
+    type: 'SET_CACHE',
+    cache: cache,
   };
 };
 
 export const setUser = data => {
   return {
-    type: "SET_USER",
-    user: data
+    type: 'SET_USER',
+    user: data,
   };
 };
 
 export const setLang = lang => {
   return {
-    type: "SET_LANG",
-    lang: lang
+    type: 'SET_LANG',
+    lang: lang,
   };
 };
 
 export const setUserAndLang = (user, lang) => {
   return {
-    type: "SET_LANG_AND_USER",
+    type: 'SET_LANG_AND_USER',
     lang: lang,
-    user: user
+    user: user,
   };
 };
 export const setStyles = styles => {
   return {
-    type: "SET_STYLES",
-    styles: styles
+    type: 'SET_STYLES',
+    styles: styles,
   };
 };
 
 export const setNotif = data => {
   return {
-    type: "SET_NOTIF",
-    notificationData: data
+    type: 'SET_NOTIF',
+    notificationData: data,
   };
 };
--- app/Redux/actions/index.js
+++ app/Redux/actions/index.js
@@ -1,1 +1,1 @@
-export * from "./globalAction";
+export * from './globalAction';
