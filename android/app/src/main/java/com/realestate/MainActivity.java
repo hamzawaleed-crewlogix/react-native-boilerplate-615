0aea8017adeccfae788a28f6c8324cfc60f09037
Hamza Waleed <hamza.waleed@crewlogix.com>
2020-01-20 15:31:59 +0500

    Project created

--- android/app/src/main/java/com/realestate/MainActivity.java
+++ android/app/src/main/java/com/realestate/MainActivity.java
@@ -1,4 +1,4 @@
-package com.realestate;
+package com.popupautomessenger;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -12,7 +12,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RealEstate";
+    return "PopUpAutoMessenger";
   }
   
   @Override
--- android/app/src/main/java/com/realestate/MainApplication.java
+++ android/app/src/main/java/com/realestate/MainApplication.java
@@ -1,4 +1,4 @@
-package com.realestate;
+package com.popupautomessenger;
 
 import android.app.Application;
 import android.content.Context;
